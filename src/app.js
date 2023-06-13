19fadd27b3552edb66728c8a83f223c069061b39
lalit-patidar <patidarlalit460@mail.com>
2023-06-13 16:06:26 +0530

    added user delete api

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.post('/add-user', (req, res) => {
     res.json({message: "user added success", ...req.body})
 })
 
+app.delete('/del-user/:userId', (req, res) => {
+    if(req.params.userId) {
+       return res.json({message: "user delete success", id: req.params.userId})
+    } 
+
+    return res.status(400).send("please give user id")
+})
+
 app.listen(PORT, () => console.log("server is runnign on port 3000"))
